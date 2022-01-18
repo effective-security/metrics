15ba19616c9e94fbc31ad4603e3b271350d51772
Denis Issoupov <dissoupov@salesforce.com>
2022-01-18 11:58:53 +0000

    Ported from dolly

---
+++ factory/factory_test.go
@@ -0,0 +1,60 @@
+package factory_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/effective-security/metrics"
+	"github.com/effective-security/metrics/factory"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func run(p metrics.Provider, times int) {
+	for i := 0; i < times; i++ {
+		p.SetGauge([]string{"test", "metrics", "gauge"}, float32(i))
+		p.IncrCounter([]string{"test", "metrics", "counter"}, float32(i))
+		p.AddSample([]string{"test", "metrics", "sample"}, float32(i))
+		p.MeasureSince([]string{"test", "metrics", "since"}, time.Now().Add(time.Duration(i)*time.Second))
+	}
+}
+
+func Test_NewMetricSinkFromURL_InMem(t *testing.T) {
+	im, err := factory.NewMetricSinkFromURL("inmem://localhost?interval=1s&retain=1m")
+	require.NoError(t, err)
+	prov, err := metrics.New(&metrics.Config{
+		FilterDefault: true,
+	}, im)
+	require.NoError(t, err)
+	run(prov, 10)
+}
+
+func Test_NewMetricSinkFromURL_InMem_InvalidParams(t *testing.T) {
+	_, err := factory.NewMetricSinkFromURL("inmem://localhost?interval=1s")
+	assert.EqualError(t, err, "bad 'retain' param: time: invalid duration \"\"")
+
+	_, err = factory.NewMetricSinkFromURL("inmem://localhost?interval=1s&retain=xxx")
+	assert.EqualError(t, err, "bad 'retain' param: time: invalid duration \"xxx\"")
+
+	_, err = factory.NewMetricSinkFromURL("inmem://localhost?retain=1s")
+	assert.EqualError(t, err, "bad 'interval' param: time: invalid duration \"\"")
+
+	_, err = factory.NewMetricSinkFromURL("inmem://localhost?retain=1s&interval=yyy")
+	assert.EqualError(t, err, "bad 'interval' param: time: invalid duration \"yyy\"")
+
+	_, err = factory.NewMetricSinkFromURL("notsupported://localhost?interval=1s")
+	assert.EqualError(t, err, "unrecognized sink name: \"notsupported\"")
+
+	_, err = factory.NewMetricSinkFromURL("^notURL::://\x7f")
+	assert.EqualError(t, err, "parse \"^notURL::://\\u007f\": net/url: invalid control character in URL")
+}
+
+func Test_NewMetricSinkFromURL_Datadog(t *testing.T) {
+	im, err := factory.NewMetricSinkFromURL("statsd://127.0.0.1:8125")
+	require.NoError(t, err)
+	prov, err := metrics.New(&metrics.Config{
+		FilterDefault: true,
+	}, im)
+	require.NoError(t, err)
+	run(prov, 10)
+}
---
+++ sink.go
@@ -0,0 +1,71 @@
+package metrics
+
+import (
+	"time"
+)
+
+// Tag is used to add dimentions to metrics
+type Tag struct {
+	Name  string
+	Value string
+}
+
+// The Sink interface is used to transmit metrics information
+// to an external system
+type Sink interface {
+	// SetGauge should retain the last value it is set to
+	SetGauge(key []string, val float32, tags []Tag)
+	// IncrCounter should accumulate values
+	IncrCounter(key []string, val float32, tags []Tag)
+	// AddSample is for timing information, where quantiles are used
+	AddSample(key []string, val float32, tags []Tag)
+}
+
+// Provider basics
+type Provider interface {
+	SetGauge(key []string, val float32, tags ...Tag)
+	IncrCounter(key []string, val float32, tags ...Tag)
+	AddSample(key []string, val float32, tags ...Tag)
+	MeasureSince(key []string, start time.Time, tags ...Tag)
+}
+
+// BlackholeSink is used to just blackhole messages
+type BlackholeSink struct{}
+
+// SetGauge should retain the last value it is set to
+func (*BlackholeSink) SetGauge(key []string, val float32, tags []Tag) {}
+
+// IncrCounter should accumulate values
+func (*BlackholeSink) IncrCounter(key []string, val float32, tags []Tag) {}
+
+// AddSample is for timing information, where quantiles are used
+func (*BlackholeSink) AddSample(key []string, val float32, tags []Tag) {}
+
+// FanoutSink is used to sink to fanout values to multiple sinks
+type FanoutSink []Sink
+
+// NewFanoutSink creates fan-out sink
+func NewFanoutSink(sinks ...Sink) FanoutSink {
+	return FanoutSink(sinks)
+}
+
+// SetGauge should retain the last value it is set to
+func (fh FanoutSink) SetGauge(key []string, val float32, tags []Tag) {
+	for _, s := range fh {
+		s.SetGauge(key, val, tags)
+	}
+}
+
+// IncrCounter should accumulate values
+func (fh FanoutSink) IncrCounter(key []string, val float32, tags []Tag) {
+	for _, s := range fh {
+		s.IncrCounter(key, val, tags)
+	}
+}
+
+// AddSample is for timing information, where quantiles are used
+func (fh FanoutSink) AddSample(key []string, val float32, tags []Tag) {
+	for _, s := range fh {
+		s.AddSample(key, val, tags)
+	}
+}
